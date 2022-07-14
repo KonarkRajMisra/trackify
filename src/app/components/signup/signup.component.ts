ca961aba6886bfaeeefa739914e7512f89990643
konarkrajmishra@gmail.com <konarkrajmisra@gmail.com>
2022-07-13 22:58:49 -0700

    adding signup page

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,24 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserDataService } from 'src/app/common/services/user-data-service/user-data-service';
+import { User } from 'src/app/common/models/user-model';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
+  providers: [UserDataService]
 })
 export class SignupComponent implements OnInit {
-  
+
   signUpForm = new FormGroup({
-    email : new FormControl('', [Validators.required, Validators.email]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     // TODO: add form validation for passwords
-    password : new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
   });
-  onSubmit(){
-    
+
+  onSubmit() {
+    const email = this.signUpForm.get('email')?.value;
+    const password = this.signUpForm.get('password')?.value;
+    console.log(email, password);
+    if (email && password) {
+      var user: User = new User(email!, password!);
+      if (user != null) {
+        console.log(this.userDataService.createUser(user))
+      }
+    }
   }
-  constructor() { }
 
   ngOnInit(): void {
   }
+  constructor(private userDataService: UserDataService) { }
 
 }
