a9fe7314cc8920c327f0b6f7ff44fc6d5b61d6af
konarkrajmishra@gmail.com <konarkrajmisra@gmail.com>
2022-07-22 16:31:47 -0700

    adding googlesignin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignupComponent } from './components/signup/signup.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { UserDataService } from './common/services/user-data-service/user-data-service';
+import { SignInComponent } from './components/signIn/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignInComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [
-    UserDataService
   ],
   bootstrap: [AppComponent]
 })
